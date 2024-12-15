385bf53a3535d580dfdf1faf218fa2993390252c
katagaki <68231035+katagaki@users.noreply.github.com>
2024-12-15 20:17:25 +0900

    Initial Commit

---
+++ IcyOverlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IcyOverlay
+//
+//  Created by シン・ジャスティン on 2024/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IcyOverlay/IcyOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcyOverlayApp.swift
+//  IcyOverlay
+//
+//  Created by シン・ジャスティン on 2024/12/15.
+//
+
+import SwiftUI
+
+@main
+struct IcyOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
